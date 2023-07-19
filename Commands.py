07e38c2b9a613c2db4a90f31b7007f15e608e0e4
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-07-19 15:26:38 +0700

    the .py files for layout and command have been created, and thepseudocode for each file have been created.

---
+++ Commands.py
@@ -0,0 +1,4 @@
+#import the tkinter module
+#import the layout class
+#create a class that inherits the methods of class undey Layout file
+#create the commands for the buttons
---
+++ Layout.py
@@ -0,0 +1,8 @@
+#This .py file contains the code for the layout of the interface
+#import the tkinter module
+#create the main window
+#create the frames
+#create the header/title
+#create the labels
+#create the entry boxes where the user will input their entry
+#create the necessary buttons
