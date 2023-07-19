fac09f9a93dd8e7c496ca2116a6ab52b1d0ad49c
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-07-19 16:37:49 +0700

    top frame and left frame have been created

--- Layout.py
+++ Layout.py
@@ -2,15 +2,25 @@
 #import the tkinter module
 from tkinter import *
 
-#create the main window
-window = Tk()
-window.geometry("1920x1200")
-window.configure(bg="#F5F5F5")
+class Layout:
+    #create the main window
+    window = Tk()
+    window.title("Safe Trace")
+    window.geometry("1420x700+50+50")
+    window.configure(bg="#F5F5F5")
 
-#create the frames
-#create the header/title
-#create the labels
-#create the entry boxes where the user will input their entry
-#create the necessary buttons
+    #create the frames
+    #top random plain header
+    header_frame=Label(window,pady=2,bd=12,bg="#C42300", justify=CENTER)
+    header_frame.pack(fill=X)
 
-mainloop()
+    #left frame
+    left_frame = LabelFrame(window, bg="#F5F5F5")
+    left_frame (x=20, y=63, width=370, height=710)
+
+    #create the header/title
+    #create the labels
+    #create the entry boxes where the user will input their entry
+    #create the necessary buttons
+
+    window.mainloop()
