0343ce5bd12d78be5a296f9d857db14b79784781
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-07-19 15:30:56 +0700

    main window has been created

--- Layout.py
+++ Layout.py
@@ -1,8 +1,16 @@
 #This .py file contains the code for the layout of the interface
 #import the tkinter module
+from tkinter import *
+
 #create the main window
+window = Tk()
+window.geometry("1920x1200")
+window.configure(bg="#F5F5F5")
+
 #create the frames
 #create the header/title
 #create the labels
 #create the entry boxes where the user will input their entry
 #create the necessary buttons
+
+mainloop()
