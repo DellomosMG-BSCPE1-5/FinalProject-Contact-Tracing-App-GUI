08542fa5839a66263b45fbe6fe572983ad99286f
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-07-25 16:15:49 +0700

    excel file has been created

--- Commands.py
+++
@@ -1,7 +0,0 @@
-#import the tkinter module
-#import the layout class
-#create a class that inherits the methods of class undey Layout file
-#create the commands for the buttons
-
-from tkinter import *
-
